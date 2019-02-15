a531bcda6d6f601abef6006784722321c9722d28
Catherine Buergel <catherinebuergel@gmail.com>
2019-02-15 16:36:53 -0700

    stuff and things

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -4,7 +4,11 @@ const itunesService = new ItunesService()
 
 function drawSongs() {
   let template = ''
-
+  let songs = itunesService.Songs
+  songs.forEach(song => {
+    template += song.getTemplate();
+  })
+  document.getElementById('songs').innerHTML = template;
   //changes button back to GET MUSIC once songs are loaded
   document.querySelector('#get-music-button').textContent = 'GET MUSIC'
   console.log(itunesService.Songs)
@@ -16,17 +20,24 @@ function drawSongs() {
 class ItunesController {
   constructor() {
     //BE SURE TO REGISTER YOUR SUBSCRIBERS!!!!!!!
+    itunesService.addSubscriber('songs', drawSongs)
   }
 
 
   //DO NOT MODIFY THIS METHOD
   getMusic(e) {
     e.preventDefault();
+    let form = e.target;
     var artist = e.target.artist.value;
     //changes the button to loading while songs load
     document.querySelector('#get-music-button').textContent = 'LOADING....'
     itunesService.getMusicByArtist(artist)
     drawSongs()
+    form.reset();
+  }
+
+  playSong(url) {
+    document.getElementById('audio').setAttribute('src', url);
   }
 }
 
--- app/models/Song.js
+++ app/models/Song.js
@@ -9,17 +9,20 @@ export default class Song {
         this.preview = song.previewUrl
     }
     getTemplate() {
-        return ` <div class="col-6">
+        return ` <div class="col-12 col-md-6 my-3">
                     <div class="card text-center">
                         <img src="${this.albumArt}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${this.title}</h5>
                             <h6>${this.artist}</h6>
-                            <audio src="${this.preview}" control="controls"
-                            <p>${this.price}</p>
+                            <p>$${this.price}</p>
+                            <button class="btn btn-info" onclick="app.controllers.itunesCtrl.playSong('${this.preview}')">Play song</button>
                         </div>
                     </div>
                 </div>`
 
     }
 }
+
+
+/* <audio controls src="${this.preview}"> </audio> */
